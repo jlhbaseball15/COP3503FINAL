16072b45ff2d5f09051e3ff5e353d0bb331622b3
Kevin Asistores <kasistores@ufl.edu>
2016-04-19 23:15:10 -0400

    added comments

--- OpenCVTest/User.h
+++ OpenCVTest/User.h
@@ -12,6 +12,17 @@ using namespace std;
 class User
 {
     
+// private:
+//     string name="";
+//     int day = 0;
+//     int month = 0;
+//     int year = 0;
+//     int feet = 0;
+//     int inches = 0;
+//     string height = "";
+//     string gender = "";
+//     string color = "";
+//     string birthday = "";
     
     
     
@@ -28,36 +39,58 @@ public:
     string birthday = "";
     
     User();
+    
     User(string name, string birthday, string height, string gender, string color);
+    
     void writeToFile();
     
     void setName(string name);
+    
     string getName();
     
     void setBirthday(int day, int month, int year);
+    
     string getBirthday();
     
+    
     int getDay();
+    
     int getMonth();
+    
     int getYear();
+    
     int getFeet();
+    
     int getInches();
+    
     void setFeet(int feet);
+    
     void setInches(int inches);
+    
+    
     
     void print();
     
+    
     void setHeight(int feet, int inches);
+    
     string getHeight();
     
+    
     void setGender(string gender);
+    
     string getGender();
     
+    
     void setColor(string color);
+    
     string getColor();
     
+    
     void setDay(int day);
+    
     void setMonth(int month);
+    
     void setYear(int year);
     
     
